78f08854dbc79b4932cd53ebe90d3809916a2f07
Rilind Pozhegu <Rilind Pozhegu>
2016-08-30 14:58:48 +0200

    nryshimi i llogos dhe panelave te ekipes

--- js/vendor/main.js
+++ js/vendor/main.js
@@ -296,109 +296,119 @@ $('#mainNav').affix({
 })
 
 
-/***************** Team More information Popup ******************/
+/***************** Team More information Popupit ******************/
 
-;( function( window ) {
+// ;( function( window ) {
 	
-	'use strict';
+// 	'use strict';
 
-	var support = { animations : Modernizr.cssanimations },
-		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
-		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
-		onEndAnimation = function( el, callback ) {
-			var onEndCallbackFn = function( ev ) {
-				if( support.animations ) {
-					if( ev.target != this ) return;
-					this.removeEventListener( animEndEventName, onEndCallbackFn );
-				}
-				if( callback && typeof callback === 'function' ) { callback.call(); }
-			};
-			if( support.animations ) {
-				el.addEventListener( animEndEventName, onEndCallbackFn );
-			}
-			else {
-				onEndCallbackFn();
-			}
-		};
+// 	var support = { animations : Modernizr.cssanimations },
+// 		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
+// 		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+// 		onEndAnimation = function( el, callback ) {
+// 			var onEndCallbackFn = function( ev ) {
+// 				if( support.animations ) {
+// 					if( ev.target != this ) return;
+// 					this.removeEventListener( animEndEventName, onEndCallbackFn );
+// 				}
+// 				if( callback && typeof callback === 'function' ) { callback.call(); }
+// 			};
+// 			if( support.animations ) {
+// 				el.addEventListener( animEndEventName, onEndCallbackFn );
+// 			}
+// 			else {
+// 				onEndCallbackFn();
+// 			}
+// 		};
 
-	function extend( a, b ) {
-		for( var key in b ) { 
-			if( b.hasOwnProperty( key ) ) {
-				a[key] = b[key];
-			}
-		}
-		return a;
-	}
+// 	function extend( a, b ) {
+// 		for( var key in b ) { 
+// 			if( b.hasOwnProperty( key ) ) {
+// 				a[key] = b[key];
+// 			}
+// 		}
+// 		return a;
+// 	}
 
-	function DialogFx( el, options ) {
-		this.el = el;
-		this.options = extend( {}, this.options );
-		extend( this.options, options );
-		this.ctrlClose = this.el.querySelector( '[data-dialog-close]' );
-		this.isOpen = false;
-		this._initEvents();
-	}
+// 	function DialogFx( el, options ) {
+// 		this.el = el;
+// 		this.options = extend( {}, this.options );
+// 		extend( this.options, options );
+// 		this.ctrlClose = this.el.querySelector( '[data-dialog-close]' );
+// 		this.isOpen = false;
+// 		this._initEvents();
+// 	}
 
-	DialogFx.prototype.options = {
-		// callbacks
-		onOpenDialog : function() { return false; },
-		onCloseDialog : function() { return false; }
-	}
+// 	DialogFx.prototype.options = {
+// 		// callbacks
+// 		onOpenDialog : function() { return false; },
+// 		onCloseDialog : function() { return false; }
+// 	}
 
-	DialogFx.prototype._initEvents = function() {
-		var self = this;
+// 	DialogFx.prototype._initEvents = function() {
+// 		var self = this;
 
-		// close action
-		this.ctrlClose.addEventListener( 'click', this.toggle.bind(this) );
+// 		// close action
+// 		this.ctrlClose.addEventListener( 'click', this.toggle.bind(this) );
 
-		// esc key closes dialog
-		document.addEventListener( 'keydown', function( ev ) {
-			var keyCode = ev.keyCode || ev.which;
-			if( keyCode === 27 && self.isOpen ) {
-				self.toggle();
-			}
-		} );
+// 		// esc key closes dialog
+// 		document.addEventListener( 'keydown', function( ev ) {
+// 			var keyCode = ev.keyCode || ev.which;
+// 			if( keyCode === 27 && self.isOpen ) {
+// 				self.toggle();
+// 			}
+// 		} );
 
-		this.el.querySelector( '.dialog__overlay' ).addEventListener( 'click', this.toggle.bind(this) );
-	}
+// 		this.el.querySelector( '.dialog__overlay' ).addEventListener( 'click', this.toggle.bind(this) );
+// 	}
 
-	DialogFx.prototype.toggle = function() {
-		var self = this;
-		if( this.isOpen ) {
-			classie.remove( this.el, 'dialog--open' );
-			classie.add( self.el, 'dialog--close' );
+// 	DialogFx.prototype.toggle = function() {
+// 		var self = this;
+// 		if( this.isOpen ) {
+// 			classie.remove( this.el, 'dialog--open' );
+// 			classie.add( self.el, 'dialog--close' );
 			
-			onEndAnimation( this.el.querySelector( '.dialog__content' ), function() {
-				classie.remove( self.el, 'dialog--close' );
-			} );
+// 			onEndAnimation( this.el.querySelector( '.dialog__content' ), function() {
+// 				classie.remove( self.el, 'dialog--close' );
+// 			} );
 
-			// callback on close
-			this.options.onCloseDialog( this );
-		}
-		else {
-			classie.add( this.el, 'dialog--open' );
+// 			// callback on close
+// 			this.options.onCloseDialog( this );
+// 		}
+// 		else {
+// 			classie.add( this.el, 'dialog--open' );
 
-			// callback on open
-			this.options.onOpenDialog( this );
-		}
-		this.isOpen = !this.isOpen;
-	};
+// 			// callback on open
+// 			this.options.onOpenDialog( this );
+// 		}
+// 		this.isOpen = !this.isOpen;
+// 	};
 
-	// add to global namespace
-	window.DialogFx = DialogFx;
+// 	// add to global namespace
+// 	window.DialogFx = DialogFx;
 
-})( window );
+// })( window );
 
-/* call */
+// /* call */
 
 
-			(function() {
+// 			(function() {
 
-				var dlgtrigger = document.querySelector( '[data-dialog]' ),
-					somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
-					dlg = new DialogFx( somedialog );
+// 				var dlgtrigger = document.querySelector( '[data-dialog]' ),
+// 					somedialog = document.getElementById( dlgtrigger.getAttribute( 'data-dialog' ) ),
+// 					dlg = new DialogFx( somedialog );
 
-				dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
+// 				dlgtrigger.addEventListener( 'click', dlg.toggle.bind(dlg) );
 
-			})();
+// 			})();
+
+
+
+
+
 		
+// POPUP BOOTSRAP
+
+$('#myModal').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
