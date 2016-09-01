ab2989339dc6b1a9ff3479feea29c62b168d2c9b
Rilind Pozhegu <Rilind Pozhegu>
2016-09-01 16:50:45 +0200

    rregullimi i struktures se kodit ndrryshim ne slider dhe dropdown navbar

--- js/vendor/main.js
+++ js/vendor/main.js
@@ -1,265 +1,266 @@
-jQuery(document).ready(function($){
-	//if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
-	var MqL = 1170;
-	//move nav element position according to window width
-	moveNavigation();
-	$(window).on('resize', function(){
-		(!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300) : window.requestAnimationFrame(moveNavigation);
-	});
+jQuery(document).ready(function($) {
+    //if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
+    var MqL = 1170;
+    //move nav element position according to window width
+    moveNavigation();
+    $(window).on('resize', function() {
+        (!window.requestAnimationFrame) ? setTimeout(moveNavigation, 300): window.requestAnimationFrame(moveNavigation);
+    });
 
-	//mobile - open lateral menu clicking on the menu icon
-	$('.cd-nav-trigger').on('click', function(event){
-		event.preventDefault();
-		if( $('.cd-main-content').hasClass('nav-is-visible') ) {
-			closeNav();
-			$('.cd-overlay').removeClass('is-visible');
-		} else {
-			$(this).addClass('nav-is-visible');
-			$('.cd-primary-nav').addClass('nav-is-visible');
-			$('.cd-main-header').addClass('nav-is-visible');
-			$('.cd-main-content').addClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-				$('body').addClass('overflow-hidden');
-			});
-			toggleSearch('close');
-			$('.cd-overlay').addClass('is-visible');
-		}
-	});
+    //mobile - open lateral menu clicking on the menu icon
+    $('.cd-nav-trigger').on('click', function(event) {
+        event.preventDefault();
+        if ($('.cd-main-content').hasClass('nav-is-visible')) {
+            closeNav();
+            $('.cd-overlay').removeClass('is-visible');
+        } else {
+            $(this).addClass('nav-is-visible');
+            $('.cd-primary-nav').addClass('nav-is-visible');
+            $('.cd-main-header').addClass('nav-is-visible');
+            $('.cd-main-content').addClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+                $('body').addClass('overflow-hidden');
+            });
+            toggleSearch('close');
+            $('.cd-overlay').addClass('is-visible');
+        }
+    });
 
-	//open search form
-	$('.cd-search-trigger').on('click', function(event){
-		event.preventDefault();
-		toggleSearch();
-		closeNav();
-	});
+    //open search form
+    $('.cd-search-trigger').on('click', function(event) {
+        event.preventDefault();
+        toggleSearch();
+        closeNav();
+    });
 
-	//close lateral menu on mobile 
-	$('.cd-overlay').on('swiperight', function(){
-		if($('.cd-primary-nav').hasClass('nav-is-visible')) {
-			closeNav();
-			$('.cd-overlay').removeClass('is-visible');
-		}
-	});
-	$('.nav-on-left .cd-overlay').on('swipeleft', function(){
-		if($('.cd-primary-nav').hasClass('nav-is-visible')) {
-			closeNav();
-			$('.cd-overlay').removeClass('is-visible');
-		}
-	});
-	$('.cd-overlay').on('click', function(){
-		closeNav();
-		toggleSearch('close')
-		$('.cd-overlay').removeClass('is-visible');
-	});
+    //close lateral menu on mobile 
+    $('.cd-overlay').on('swiperight', function() {
+        if ($('.cd-primary-nav').hasClass('nav-is-visible')) {
+            closeNav();
+            $('.cd-overlay').removeClass('is-visible');
+        }
+    });
+    $('.nav-on-left .cd-overlay').on('swipeleft', function() {
+        if ($('.cd-primary-nav').hasClass('nav-is-visible')) {
+            closeNav();
+            $('.cd-overlay').removeClass('is-visible');
+        }
+    });
+    $('.cd-overlay').on('click', function() {
+        closeNav();
+        toggleSearch('close')
+        $('.cd-overlay').removeClass('is-visible');
+    });
 
 
-	//prevent default clicking on direct children of .cd-primary-nav 
-	$('.cd-primary-nav').children('.has-children').children('a').on('click', function(event){
-		event.preventDefault();
-	});
-	//open submenu
-	$('.has-children').children('a').on('click', function(event){
-		if( !checkWindowWidth() ) event.preventDefault();
-		var selected = $(this);
-		if( selected.next('ul').hasClass('is-hidden') ) {
-			//desktop version only
-			selected.addClass('selected').next('ul').removeClass('is-hidden').end().parent('.has-children').parent('ul').addClass('moves-out');
-			selected.parent('.has-children').siblings('.has-children').children('ul').addClass('is-hidden').end().children('a').removeClass('selected');
-			$('.cd-overlay').addClass('is-visible');
-		} else {
-			selected.removeClass('selected').next('ul').addClass('is-hidden').end().parent('.has-children').parent('ul').removeClass('moves-out');
-			$('.cd-overlay').removeClass('is-visible');
-		}
-		toggleSearch('close');
-	});
+    //prevent default clicking on direct children of .cd-primary-nav 
+    $('.cd-primary-nav').children('.has-children').children('a').on('click', function(event) {
+        event.preventDefault();
+    });
+    //open submenu
+    $('.has-children').children('a').on('click', function(event) {
+        if (!checkWindowWidth()) event.preventDefault();
+        var selected = $(this);
+        if (selected.next('ul').hasClass('is-hidden')) {
+            //desktop version only
+            selected.addClass('selected').next('ul').removeClass('is-hidden').end().parent('.has-children').parent('ul').addClass('moves-out');
+            selected.parent('.has-children').siblings('.has-children').children('ul').addClass('is-hidden').end().children('a').removeClass('selected');
+            $('.cd-overlay').addClass('is-visible');
+        } else {
+            selected.removeClass('selected').next('ul').addClass('is-hidden').end().parent('.has-children').parent('ul').removeClass('moves-out');
+            $('.cd-overlay').removeClass('is-visible');
+        }
+        toggleSearch('close');
+    });
 
-	//submenu items - go back link
-	$('.go-back').on('click', function(){
-		$(this).parent('ul').addClass('is-hidden').parent('.has-children').parent('ul').removeClass('moves-out');
-	});
+    //submenu items - go back link
+    $('.go-back').on('click', function() {
+        $(this).parent('ul').addClass('is-hidden').parent('.has-children').parent('ul').removeClass('moves-out');
+    });
 
-	function closeNav() {
-		$('.cd-nav-trigger').removeClass('nav-is-visible');
-		$('.cd-main-header').removeClass('nav-is-visible');
-		$('.cd-primary-nav').removeClass('nav-is-visible');
-		$('.has-children ul').addClass('is-hidden');
-		$('.has-children a').removeClass('selected');
-		$('.moves-out').removeClass('moves-out');
-		$('.cd-main-content').removeClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-			$('body').removeClass('overflow-hidden');
-		});
-	}
+    function closeNav() {
+        $('.cd-nav-trigger').removeClass('nav-is-visible');
+        $('.cd-main-header').removeClass('nav-is-visible');
+        $('.cd-primary-nav').removeClass('nav-is-visible');
+        $('.has-children ul').addClass('is-hidden');
+        $('.has-children a').removeClass('selected');
+        $('.moves-out').removeClass('moves-out');
+        $('.cd-main-content').removeClass('nav-is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+            $('body').removeClass('overflow-hidden');
+        });
+    }
 
-	function toggleSearch(type) {
-		if(type=="close") {
-			//close serach 
-			$('.cd-search').removeClass('is-visible');
-			$('.cd-search-trigger').removeClass('search-is-visible');
-			$('.cd-overlay').removeClass('search-is-visible');
-		} else {
-			//toggle search visibility
-			$('.cd-search').toggleClass('is-visible');
-			$('.cd-search-trigger').toggleClass('search-is-visible');
-			$('.cd-overlay').toggleClass('search-is-visible');
-			if($(window).width() > MqL && $('.cd-search').hasClass('is-visible')) $('.cd-search').find('input[type="search"]').focus();
-			($('.cd-search').hasClass('is-visible')) ? $('.cd-overlay').addClass('is-visible') : $('.cd-overlay').removeClass('is-visible') ;
-		}
-	}
+    function toggleSearch(type) {
+        if (type == "close") {
+            //close serach 
+            $('.cd-search').removeClass('is-visible');
+            $('.cd-search-trigger').removeClass('search-is-visible');
+            $('.cd-overlay').removeClass('search-is-visible');
+        } else {
+            //toggle search visibility
+            $('.cd-search').toggleClass('is-visible');
+            $('.cd-search-trigger').toggleClass('search-is-visible');
+            $('.cd-overlay').toggleClass('search-is-visible');
+            if ($(window).width() > MqL && $('.cd-search').hasClass('is-visible')) $('.cd-search').find('input[type="search"]').focus();
+            ($('.cd-search').hasClass('is-visible')) ? $('.cd-overlay').addClass('is-visible'): $('.cd-overlay').removeClass('is-visible');
+        }
+    }
 
-	function checkWindowWidth() {
-		//check window width (scrollbar included)
-		var e = window, 
+    function checkWindowWidth() {
+        //check window width (scrollbar included)
+        var e = window,
             a = 'inner';
-        if (!('innerWidth' in window )) {
+        if (!('innerWidth' in window)) {
             a = 'client';
             e = document.documentElement || document.body;
         }
-        if ( e[ a+'Width' ] >= MqL ) {
-			return true;
-		} else {
-			return false;
-		}
-	}
+        if (e[a + 'Width'] >= MqL) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	function moveNavigation(){
-		var navigation = $('.cd-nav');
-  		var desktop = checkWindowWidth();
-        if ( desktop ) {
-			navigation.detach();
-			navigation.insertBefore('.cd-header-buttons');
-		} else {
-			navigation.detach();
-			navigation.insertAfter('.cd-main-content');
-		}
-	}
+    function moveNavigation() {
+        var navigation = $('.cd-nav');
+        var desktop = checkWindowWidth();
+        if (desktop) {
+            navigation.detach();
+            navigation.insertBefore('.cd-header-buttons');
+        } else {
+            navigation.detach();
+            navigation.insertAfter('.cd-main-content');
+        }
+    }
 });
 
 
 //  Maps code
-          $('.map-container')
-            .click(function(){
-                $(this).find('iframe').addClass('clicked')})
-            .mouseleave(function(){
-                $(this).find('iframe').removeClass('clicked')});
+$('.map-container')
+    .click(function() {
+        $(this).find('iframe').addClass('clicked')
+    })
+    .mouseleave(function() {
+        $(this).find('iframe').removeClass('clicked')
+    });
 
 
 // TEAM PopUP
 
-( function( window ) {
+(function(window) {
 
-'use strict';
+    'use strict';
 
-// class helper functions from bonzo https://github.com/ded/bonzo
+    // class helper functions from bonzo https://github.com/ded/bonzo
 
-function classReg( className ) {
-  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
-}
-
-// classList support for class management
-// altho to be fair, the api sucks because it won't accept multiple classes at once
-var hasClass, addClass, removeClass;
-
-if ( 'classList' in document.documentElement ) {
-  hasClass = function( elem, c ) {
-    return elem.classList.contains( c );
-  };
-  addClass = function( elem, c ) {
-    elem.classList.add( c );
-  };
-  removeClass = function( elem, c ) {
-    elem.classList.remove( c );
-  };
-}
-else {
-  hasClass = function( elem, c ) {
-    return classReg( c ).test( elem.className );
-  };
-  addClass = function( elem, c ) {
-    if ( !hasClass( elem, c ) ) {
-      elem.className = elem.className + ' ' + c;
+    function classReg(className) {
+        return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
     }
-  };
-  removeClass = function( elem, c ) {
-    elem.className = elem.className.replace( classReg( c ), ' ' );
-  };
-}
 
-function toggleClass( elem, c ) {
-  var fn = hasClass( elem, c ) ? removeClass : addClass;
-  fn( elem, c );
-}
+    // classList support for class management
+    // altho to be fair, the api sucks because it won't accept multiple classes at once
+    var hasClass, addClass, removeClass;
 
-var classie = {
-  // full names
-  hasClass: hasClass,
-  addClass: addClass,
-  removeClass: removeClass,
-  toggleClass: toggleClass,
-  // short names
-  has: hasClass,
-  add: addClass,
-  remove: removeClass,
-  toggle: toggleClass
-};
+    if ('classList' in document.documentElement) {
+        hasClass = function(elem, c) {
+            return elem.classList.contains(c);
+        };
+        addClass = function(elem, c) {
+            elem.classList.add(c);
+        };
+        removeClass = function(elem, c) {
+            elem.classList.remove(c);
+        };
+    } else {
+        hasClass = function(elem, c) {
+            return classReg(c).test(elem.className);
+        };
+        addClass = function(elem, c) {
+            if (!hasClass(elem, c)) {
+                elem.className = elem.className + ' ' + c;
+            }
+        };
+        removeClass = function(elem, c) {
+            elem.className = elem.className.replace(classReg(c), ' ');
+        };
+    }
 
-// transport
-if ( typeof define === 'function' && define.amd ) {
-  // AMD
-  define( classie );
-} else {
-  // browser global
-  window.classie = classie;
-}
+    function toggleClass(elem, c) {
+        var fn = hasClass(elem, c) ? removeClass : addClass;
+        fn(elem, c);
+    }
 
-})( window );
+    var classie = {
+        // full names
+        hasClass: hasClass,
+        addClass: addClass,
+        removeClass: removeClass,
+        toggleClass: toggleClass,
+        // short names
+        has: hasClass,
+        add: addClass,
+        remove: removeClass,
+        toggle: toggleClass
+    };
+
+    // transport
+    if (typeof define === 'function' && define.amd) {
+        // AMD
+        define(classie);
+    } else {
+        // browser global
+        window.classie = classie;
+    }
+
+})(window);
 
 
 // Modal Effects TEAM POPUP
 
 var ModalEffects = (function() {
 
-	function init() {
+    function init() {
 
-		var overlay = document.querySelector( '.md-overlay' );
+        var overlay = document.querySelector('.md-overlay');
 
-		[].slice.call( document.querySelectorAll( '.md-trigger' ) ).forEach( function( el, i ) {
+        [].slice.call(document.querySelectorAll('.md-trigger')).forEach(function(el, i) {
 
-			var modal = document.querySelector( '#' + el.getAttribute( 'data-modal' ) ),
-				close = modal.querySelector( '.md-close' );
+            var modal = document.querySelector('#' + el.getAttribute('data-modal')),
+                close = modal.querySelector('.md-close');
 
-			function removeModal( hasPerspective ) {
-				classie.remove( modal, 'md-show' );
+            function removeModal(hasPerspective) {
+                classie.remove(modal, 'md-show');
 
-				if( hasPerspective ) {
-					classie.remove( document.documentElement, 'md-perspective' );
-				}
-			}
+                if (hasPerspective) {
+                    classie.remove(document.documentElement, 'md-perspective');
+                }
+            }
 
-			function removeModalHandler() {
-				removeModal( classie.has( el, 'md-setperspective' ) ); 
-			}
+            function removeModalHandler() {
+                removeModal(classie.has(el, 'md-setperspective'));
+            }
 
-			el.addEventListener( 'click', function( ev ) {
-				classie.add( modal, 'md-show' );
-				overlay.removeEventListener( 'click', removeModalHandler );
-				overlay.addEventListener( 'click', removeModalHandler );
+            el.addEventListener('click', function(ev) {
+                classie.add(modal, 'md-show');
+                overlay.removeEventListener('click', removeModalHandler);
+                overlay.addEventListener('click', removeModalHandler);
 
-				if( classie.has( el, 'md-setperspective' ) ) {
-					setTimeout( function() {
-						classie.add( document.documentElement, 'md-perspective' );
-					}, 25 );
-				}
-			});
+                if (classie.has(el, 'md-setperspective')) {
+                    setTimeout(function() {
+                        classie.add(document.documentElement, 'md-perspective');
+                    }, 25);
+                }
+            });
 
-			close.addEventListener( 'click', function( ev ) {
-				ev.stopPropagation();
-				removeModalHandler();
-			});
+            close.addEventListener('click', function(ev) {
+                ev.stopPropagation();
+                removeModalHandler();
+            });
 
-		} );
+        });
 
-	}
+    }
 
-	init();
+    init();
 
 })();
 
@@ -268,19 +269,19 @@ var ModalEffects = (function() {
 
 $(function() {
 
-	$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html,body').animate({
-					scrollTop: target.offset().top
-				}, 1900);
-				return false;
-			}
-		}
-	});
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1900);
+                return false;
+            }
+        }
+    });
 
 });
 
@@ -290,16 +291,25 @@ $(function() {
 
 
 $('#mainNav').affix({
-  offset: {
-    top: 60
-  }
+    offset: {
+        top: 60
+    }
 })
 
 
 
-		
+
 /***************** POPUP BOOTSRAP ******************/
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').focus()
+$('#myModal').on('shown.bs.modal', function() {
+    $('#myInput').focus()
 })
+
+
+/***************** DROP DOWN HOVER ******************/
+
+$('ul.nav li.dropdown').hover(function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(300);
+}, function() {
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(300);
+});
